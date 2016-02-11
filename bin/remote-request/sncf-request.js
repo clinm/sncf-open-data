004740609be386b004b98b09e5ba33a3b9046d06
Matthieu <Matthieu.clin@wanadoo.fr>
2016-02-11 21:38:18 +0100

    Implementation of basic methods to retrieve data from sncf API
    Getting data from the remote API will be one of the key element of
    this project. That's why two methods has been implemented.
    
    The first one is very basic and just performs a get request by creating
    the url and sending data. It should be the only method to actually
    performs GET request.
    
    The second one was created in order to fill the missing functionality of
    the navitia API. In fact, we can't retrieve all records with one request.
    That's why this method makes multiple requests in order to get all data
    and concat them as one final response.

--- bin/remote-request/sncf-request.js
+++ bin/remote-request/sncf-request.js
@@ -4,10 +4,141 @@
 var sncfRequest = function(){
     var CONF= require("./CONF.json");
 
-    var sncf = {};
+    var request = require('request');
+    var _ = require("underscore");
+    var async = require('async');
 
+    var is_array = function (value) {
+        return value && typeof value === 'object'
+            && typeof value.length === 'number'
+            && typeof value.splice === 'function' &&
+            !(value.propertyIsEnumerable('length'));
+    };
+
+    var sncf = {
+        /**
+         *  Clean all useless parameters from the response (links etc).
+         *  Used by getAll in order to concat all requests
+         * @param body  Response from a get method. Can be String or Object
+         * @returns {*} The response cleaned from disruptions/pagination, links and feed_publishers
+         */
+        cleanResponse: function(body){
+            if(typeof body === 'string'){
+                body = JSON.parse(body);
+            }
+
+            delete body.disruptions;
+            delete body.pagination;
+            delete body.links;
+            delete body.feed_publishers;
+
+            return body;
+        },
+        /**
+         *  Simply performs a get request to the remote API
+         * @param {string} url       resources' path
+         * @param {Object} params    parameters that will be passed as GET parameters
+         * @param callback  same callback as {@link request.get}
+         */
+        get: function(url, params, callback){
+            if(url.indexOf('/') > 0 || url.indexOf('/') == -1){
+                url = '/' + url;
+            }
+
+            var request_url={
+                url: 'https://'+CONF.KEY+'@' + CONF.BASE_URL + url,
+                formData: params
+            };
+
+            request.get(request_url, callback);
+        },
+        /**
+         *  Performs all request with start_page property from start to end.
+         *  Concat all responses with seed. Two cases:
+         *      - Everything went well: callback(null, null, concatenatedResult);
+         *      - Something went wrong: callback(error, null, errorReceived);
+         * @param {string} url
+         * @param {Object} params
+         * @param {!number} start               Beginning number for start_page
+         * @param {!number} countPerRequest     Fill the count property of params
+         * @param {!number} end                 End number for start
+         * @param callback                      Called when all request has been made and the result concatenated
+         * @param {[]} seed                     Seed for concatenation, if none is provided then [] will be used
+         */
+        performsAllRequest: function(url, params, start, countPerRequest, end, callback, seed){
+            async.map(_.range(start, end), function(page, callbackMap){
+                var local = {};
+                Object.assign(local, params);
+                local.start_page = page;
+                local.count = countPerRequest;
+                sncf.get(url, local, function(error, response, body){
+                    if (!error && response.statusCode == 200) {
+                        callbackMap(null, sncf.cleanResponse(body));
+                    }else{
+                        callbackMap(error, body);
+                    }
+                });
+
+            }, function(error, result){
+                if(!error){
+                    if(!seed){
+                        seed = [];
+                    }
+                    result = _.reduce(result, function(memo, elt){
+                        elt = sncf.cleanResponse(elt);
+                        return memo.concat(elt[Object.keys(elt)[0]]);
+                    }, seed);
+                }else{
+                    console.log("sncf errro");
+                    console.log(error);
+                    console.log(result);
+                }
+                callback(error, null, result);
+            });
+        },
+        /**
+         * The remote API does not provide a way to retrieve all records for a given request. Thus,
+         * this method allows to retrieve all record by making multiple request and join them at the end.
+         * params.count parameter will be override if needed
+         * @param url       resources' path
+         * @param params    parameters that ill be passed as GET parameters
+         * @param callback  same callback as {@link request.get}
+         */
+        getAll: function(url, params, callback){
+            // count parameter for the request
+            var count = 1000;
+            params = params || {};
+            params.count = count;
+
+            var checkReceivedAll = function(error, response, body){
+                if (!error && response.statusCode == 200) {
+                    var data = JSON.parse(body);
+                    var total = data.pagination.total_result;
+                    if(data.pagination.items_on_page === total){
+                        callback(error, response, body);
+                    }else{
+                        // compute how many request have to be sent
+                        var itemsOnPage = data.pagination.items_on_page;
+                        var requestNumber = total/ itemsOnPage;
+
+                        data = sncf.cleanResponse(data);
+                        data = data[Object.keys(data)[0]];
+                        if(!is_array(data)){
+                            data = [data];
+                        }
+                        sncf.performsAllRequest(url, params, 1, count, requestNumber, callback, data);
+                    }
+                }else{
+                    console.log("An error occurred while performing getAll");
+                    callback(error, response, body);
+                }
+            };
+
+            sncf.get(url, params, checkReceivedAll);
+        }
+    };
 
     return sncf;
 };
 
-exports.sncfRequest = sncfRequest();
+module.exports = sncfRequest();
--- test/remote-request/basicTest.js
+++ test/remote-request/basicTest.js
@@ -3,16 +3,178 @@ var fs = require("fs");
 
 var bin_directory = path.join(__dirname, "../../bin");
 var SNCF_FILE = path.join(bin_directory, "remote-request", "sncf-request");
-var CONF_FILE = path.join(bin_directory, "remote-request", "CONF.json");
 
 var chai = require("chai");
-
+var sinon = require("sinon");
 var expect = chai.expect;
+var request = require("request");
+var sandbox;
+
+var sncf = require(SNCF_FILE);
 
 describe("Basic behaviour", function(){
-    it("should load the configuration file", function(){
-        fs.renameSync(CONF_FILE, CONF_FILE+".old");
-        expect(function(){require(SNCF_FILE)}).to.throw("Cannot find module './CONF.json'");
-        fs.renameSync(CONF_FILE+".old", CONF_FILE);
-    })
+    var spy;
+
+    beforeEach(function () {
+        sandbox = sinon.sandbox.create();
+        spy = sandbox.stub(request, 'get');
+    });
+
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+
+    describe("get function", function(){
+
+        it("should call callback", function(done){
+            spy.yields();
+            sncf.get("stop_areas", {}, done);
+        });
+
+        it("should call request.get", function(done){
+            var msg = {dummy: "dummyContent"};
+            var expected = {message: "Well done"};
+            spy.yields(null, null, JSON.stringify(expected));
+            sncf.get("stop_areas", {dummy: "dummyContent"}, function(error, response, body){
+                expect(body).equals(JSON.stringify(expected));
+                expect(spy.getCall(0).args[0].formData.message).equals(msg.message);
+                done();
+            });
+        });
+
+        it("should handle '/' at the beginning", function(done){
+            var expected = {message: "Well done"};
+            spy.yields(null, null, JSON.stringify(expected));
+
+            var args= ["stop_areas/", "stop_areas/", "/stop_areas"];
+            // call with and without '/'
+            sncf.get(args[0], {dummy: "dummyContent"}, function(){
+                sncf.get(args[1], {dummy: "dummyContent"}, function(){
+                    sncf.get(args[2], {dummy: "dummyContent"}, function(){
+                       for(var i = 0; i < spy.callCount; i++){
+                           var url = spy.getCall(i).args[0].url;
+                           expect(url.indexOf(args[i])).to.be.equals(url.length - args[i].length);
+                       }
+                        done();
+                    });
+                });
+            });
+        });
+    });
+
+    describe("getAll function", function(){
+        it("should not call a second time when everything has been received", function(done){
+            var response = {pagination: {items_on_page: 30, total_result: 30}};
+            spy.yields(null, {statusCode: 200}, JSON.stringify(response));
+
+            sncf.getAll("dummy", {}, function(){
+                //noinspection JSUnresolvedVariable,BadExpressionStatementJS
+                expect(spy.calledOnce).to.be.true;
+                done();
+            });
+        });
+
+        it("should perform all requests needed (total = k*1000 + r)", function(done){
+            // should have 1 request at the beginning and then 3 more
+            // 1*1000 + (2*1000 + 1*15) = 3015
+            var response = {pagination: {items_on_page: 1000, total_result: 3015}};
+            spy.yields(null, {statusCode: 200}, JSON.stringify(response));
+
+            sncf.getAll("dummy", {}, function(){
+                expect(spy.callCount).equals(4);
+                var startPages = [];
+                for(var i = 1; i < spy.callCount; i++){
+                    var elt = spy.getCall(i);
+                    startPages[elt.args[0].formData.start_page] = true;
+                }
+
+                for(i = 1; i < 4; i++){
+                    //noinspection BadExpressionStatementJS
+                    expect(startPages[i]).to.be.true;
+                }
+
+                done();
+            })
+
+        });
+
+
+        it("should perform all requests needed (total = k*1000)", function(done){
+            // should have 1 request at the beginning and then 2 more
+            // 1*1000 + (2*1000) = 3000
+            var response = {pagination: {items_on_page: 1000, total_result: 3000}};
+            spy.yields(null, {statusCode: 200}, JSON.stringify(response));
+
+            sncf.getAll("dummy", {}, function(){
+                expect(spy.callCount).equals(3);
+                var startPages = [];
+                for(var i = 1; i < spy.callCount; i++){
+                    var elt = spy.getCall(i);
+                    startPages[elt.args[0].formData.start_page] = true;
+                }
+
+                for(i = 1; i < 3; i++){
+                    //noinspection BadExpressionStatementJS
+                    expect(startPages[i]).to.be.true;
+                }
+
+                done();
+            })
+
+        });
+    });
+
+    describe("performAllRequest", function(){
+        it("should do nothing when start > end", function(done){
+            var response = {pagination: {items_on_page: 30, total_result: 30}};
+            spy.yields(null, {statusCode: 200}, JSON.stringify(response));
+            sncf.performsAllRequest("url", {}, 10, -100, 0, function(){
+                expect(spy.callCount).to.be.equal(0);
+            }, []);
+            done();
+        });
+
+        it("should use [] as default seed", function(done){
+            var response = {result: {message: "My message"}};
+            spy.yields(null, {statusCode: 200}, JSON.stringify(response));
+            //noinspection JSCheckFunctionSignatures
+            sncf.performsAllRequest("url", {}, 1, 10, 3, function(error, response, body){
+                expect(spy.callCount).to.be.equal(2);
+                //noinspection BadExpressionStatementJS
+                expect(body).to.be.exist;
+                expect(body.length).to.be.equal(2);
+            });
+            done();
+        });
+
+        it("should return error", function(done){
+            var expected = {result: {message: "My error message"}};
+            spy.yields(null, {statusCode: 200}, JSON.stringify(expected));
+            sncf.performsAllRequest("url", {}, 1, 10, 2, function(error, response, body){
+                expect(spy.callCount).to.be.equal(1);
+                //noinspection BadExpressionStatementJS
+                expect(body).to.be.exist;
+                expect(body).to.be.deep.equal([expected.result]);
+                done();
+            }, []);
+        });
+    });
+
+    describe("cleanResponse", function(){
+        it("should clean response", function(){
+            var response = {
+                "disruptions": [],
+                "pagination": {},
+                "links": [],
+                "feed_publishers": []
+            };
+
+            var responseAsString = JSON.stringify(response);
+
+            expect(sncf.cleanResponse(response)).to.deep.equal({});
+            expect(sncf.cleanResponse(responseAsString)).to.deep.equal({});
+        });
+    });
+
 });
